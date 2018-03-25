7a2af125a917a2fbc337f6b88bb989f4494a8314
Wilson Freitas <wilson.freitas@gmail.com>
2018-03-25 14:01:30 +0000

    version 1.0.5

--- R/bizdays.R
+++ R/bizdays.R
@@ -79,7 +79,12 @@ bizdays.Date <- function(from, to, cal=bizdays.options$get('default.calendar')) 
   new.to <- cal$adjust.to(new.to, cal)
   bdays <- cal$bizdays(as.integer(new.from), as.integer(new.to))
   bdays[idx] <- -bdays[idx]
-  bdays
+  adj_vec <- as.integer( !(cal$is.bizday(as.integer(new.from)) | cal$is.bizday(as.integer(new.to))) )
+  bdays <- bdays - adj_vec
+  if (cal$financial)
+    bdays
+  else
+    bdays + 1
 }
 
 #' Business days and current days equivalence
